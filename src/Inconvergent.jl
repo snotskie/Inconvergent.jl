a1daec36dc0bfdb0dbd6c0c9003a255ae83f592a
Bryan A. Knowles <snotskie@richard-parker.lan>
2020-04-15 14:33:27 -0500

    initial 3

--- src/Inconvergent.jl
+++ src/Inconvergent.jl
@@ -11,4 +11,9 @@ include("./sandpaint.jl")
 include("./weir.jl")
 include("./examples.jl")
 
+export sample_points_from_line, random_point_in_circle, normal_point_in_circle
+export Sandpaint, stroke, points, Base.display
+export Weir, edges, add_edge!, get_vertex, get_vertices, add_vertex!, move_vertex_by!, move_vertex_to!
+export lines_example
+
 end # module
